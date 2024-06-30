a6977b7f654d57b1875e863718b3eba8f524d78f
wjdsheep15 <wjd15sheep@gmail.com>
2024-06-30 15:28:22 +0900

    Feat Firest Commit

---
+++ python.py
@@ -0,0 +1,13 @@
+a, b = 3, 2
+a *= b   # a = 6
+print(a)
+a **= b    # a= 36
+print(a)
+print(type(a))
+a /= b     # a = 18.0
+print(type(a))
+print(a)
+a //= b    # a= 9.0
+print(a)
+a %= b     # a = 1.0
+print(a)
