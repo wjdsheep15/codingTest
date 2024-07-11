b92bfe2a44ba3322490cdd50c4a56e7a15be7696
wjdsheep15 <wjd15sheep@gmail.com>
2024-07-11 17:40:25 +0900

    [FEAT]-백준 2606 바이러스 문제 풀이중]

---
+++ backjoon/BFS/2606바이러스.py
@@ -0,0 +1,53 @@
+'''
+신종 바이러스인 웜 바이러스는 네트워크를 통해 전파된다. 한 컴퓨터가 웜 바이러스에 걸리면 그 컴퓨터와 네트워크 상에서 연결되어 있는 모든 컴퓨터는 웜 바이러스에 걸리게 된다.
+
+예를 들어 7대의 컴퓨터가 <그림 1>과 같이 네트워크 상에서 연결되어 있다고 하자. 1번 컴퓨터가 웜 바이러스에 걸리면 웜 바이러스는 2번과 5번 컴퓨터를 거쳐 3번과 6번 컴퓨터까지 전파되어 2, 3, 5, 6 네 대의 컴퓨터는 웜 바이러스에 걸리게 된다. 하지만 4번과 7번 컴퓨터는 1번 컴퓨터와 네트워크상에서 연결되어 있지 않기 때문에 영향을 받지 않는다.
+
+
+
+어느 날 1번 컴퓨터가 웜 바이러스에 걸렸다. 컴퓨터의 수와 네트워크 상에서 서로 연결되어 있는 정보가 주어질 때, 1번 컴퓨터를 통해 웜 바이러스에 걸리게 되는 컴퓨터의 수를 출력하는 프로그램을 작성하시오.
+
+입력
+7
+6
+1 2
+2 3
+1 5
+5 2
+5 6
+4 7
+
+출력
+4
+'''
+
+
+# 재귀함수
+def dfs(x, count):
+    visited[x] = True
+    for node in graph[x]:
+        if not visited[node]:
+            count = dfs(node, count+1)
+    return count
+
+
+v = int(input())
+e = int(input())
+visited = [False for _ in range(v+1)]
+graph = [[] for _ in range(v+1)]
+
+for _ in range(e):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+print(dfs(1, 0))
+print(visited)
+
+
+#  0      1        2       3    4       5       6    7
+# [[], [2, 5], [1, 3, 5], [2], [7], [1, 2, 6], [5], [4]]
+
+
+# visited
+# [False, True, True, True, False, True, True, False]
--- backjoon/String/programmers문자열압축.py
+++ backjoon/String/programmers문자열압축.py
@@ -1,5 +1,38 @@
 '''
 압축할 문자열 s가 매개변수로 주어질 때, 위에 설명한 방법으로 1개 이상 단위로 문자열을 잘라 압축하여 표현한 문자열 중 가장 짧은 것의 길이를 return 하도록 solution 함수를 완성해주세요.
+
+def compress(text, tok_len):
+    words = [text[i:i+tok_len] for i in range(0, len(text), tok_len)]
+    res = []
+    cur_word = words[0]
+    cur_cnt = 1
+    for a, b in zip(words, words[1:] + ['']):
+        if a == b:
+            cur_cnt += 1
+        else:
+            res.append([cur_word, cur_cnt])
+            cur_word = b
+            cur_cnt = 1
+    return sum(len(word) + (len(str(cnt)) if cnt > 1 else 0) for word, cnt in res)
+
+def solution(text):
+    return min(compress(text, tok_len) for tok_len in list(range(1, int(len(text)/2) + 1)) + [len(text)])
+
+a = [
+    "aabbaccc",
+    "ababcdcdababcdcd",
+    "abcabcdede",
+    "abcabcabcabcdededededede",
+    "xababcdcdababcdcd",
+
+    'aaaaaa',
+]
+
+for x in a:
+    print(solution(x))
+
+
+
 '''
 
 
