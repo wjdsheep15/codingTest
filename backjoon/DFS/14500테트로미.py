206dcaf47f4294bc7e01585e32055e28ee700e78
wjdsheep15 <wjd15sheep@gmail.com>
2025-03-26 15:15:19 +0900

    Feat: 14500

---
+++ backjoon/DFS/14500테트로미.py
@@ -0,0 +1,101 @@
+'''
+문제
+폴리오미노란 크기가 1×1인 정사각형을 여러 개 이어서 붙인 도형이며, 다음과 같은 조건을 만족해야 한다.
+
+정사각형은 서로 겹치면 안 된다.
+도형은 모두 연결되어 있어야 한다.
+정사각형의 변끼리 연결되어 있어야 한다. 즉, 꼭짓점과 꼭짓점만 맞닿아 있으면 안 된다.
+정사각형 4개를 이어 붙인 폴리오미노는 테트로미노라고 하며, 다음과 같은 5가지가 있다.
+
+
+
+아름이는 크기가 N×M인 종이 위에 테트로미노 하나를 놓으려고 한다. 종이는 1×1 크기의 칸으로 나누어져 있으며, 각각의 칸에는 정수가 하나 쓰여 있다.
+
+테트로미노 하나를 적절히 놓아서 테트로미노가 놓인 칸에 쓰여 있는 수들의 합을 최대로 하는 프로그램을 작성하시오.
+
+테트로미노는 반드시 한 정사각형이 정확히 하나의 칸을 포함하도록 놓아야 하며, 회전이나 대칭을 시켜도 된다.
+
+입력
+첫째 줄에 종이의 세로 크기 N과 가로 크기 M이 주어진다. (4 ≤ N, M ≤ 500)
+
+둘째 줄부터 N개의 줄에 종이에 쓰여 있는 수가 주어진다. i번째 줄의 j번째 수는 위에서부터 i번째 칸, 왼쪽에서부터 j번째 칸에 쓰여 있는 수이다. 입력으로 주어지는 수는 1,000을 넘지 않는 자연수이다.
+
+출력
+첫째 줄에 테트로미노가 놓인 칸에 쓰인 수들의 합의 최댓값을 출력한다.
+
+예제 입력 1
+5 5
+1 2 3 4 5
+5 4 3 2 1
+2 3 4 5 6
+6 5 4 3 2
+1 2 1 2 1
+예제 출력 1
+19
+'''
+
+import sys
+input = sys.stdin.readline
+
+# 상, 하, 좌, 우
+move = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+# INPUT
+N, M = map(int, input().split())
+board = [list(map(int,input().split())) for _ in range(N)]
+visited = [[False] * M for _ in range(N)]
+
+# 최대값 변수
+maxValue = 0
+
+# ㅗ, ㅜ, ㅓ, ㅏ 제외한 모양들 최대값 계산
+def dfs(i, j, dsum, cnt):
+    global maxValue
+    # 모양 완성되었을 때 최대값 계산
+    if cnt == 4:
+        maxValue = max(maxValue, dsum)
+        return
+
+    # 상, 하, 좌, 우로 이동
+    for n in range(4):
+        ni = i+move[n][0]
+        nj = j+move[n][1]
+        if 0 <= ni < N and 0 <= nj < M and not visited[ni][nj]:
+            # 방문 표시 및 제거
+            visited[ni][nj] = True
+            dfs(ni, nj, dsum + board[ni][nj], cnt+1)
+            visited[ni][nj] = False
+
+
+# ㅗ, ㅜ, ㅓ, ㅏ 모양의 최대값 계산
+def exce(i, j):
+    global maxValue
+    for n in range(4):
+        # 초기값은 시작지점의 값으로 지정
+        tmp = board[i][j]
+        for k in range(3):
+            # move 배열의 요소를 3개씩 사용할 수 있도록 인덱스 계산
+            # 012, 123, 230, 301
+            t = (n+k)%4
+            ni = i+move[t][0]
+            nj = j+move[t][1]
+
+            if not (0 <= ni < N and 0 <= nj < M):
+                tmp = 0
+                break
+            tmp += board[ni][nj]
+        # 최대값 계산
+        maxValue = max(maxValue, tmp)
+
+
+for i in range(N):
+    for j in range(M):
+        # 시작점 visited 표시
+        visited[i][j] = True
+        dfs(i, j, board[i][j], 1)
+        visited[i][j] = False
+
+        exce(i, j)
+
+print(maxValue)
+
